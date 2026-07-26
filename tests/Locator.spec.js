dd9520b1d10d1b4513e602bfcf7efb8cd710e2f3
Sunil <sunilrawatje2001@gmail.com>
2026-07-26 20:33:33 +0530

    getting ele count

--- tests/Locator.spec.js
+++ tests/Locator.spec.js
@@ -1,10 +1,33 @@
 import {test,expect} from '@playwright/test'
 
 test('Locators',async({page})=>{
+    //enter url
     await page.goto('https://www.saucedemo.com/')
+    //enter user-name
     await page.locator('id=user-name').fill('standard_user')
+    //enter password
     await page.locator('#password').fill('secret_sauce')
+    //click on login button
     await page.locator('//input[@id="login-button"]').click()
+    //find url to have particular word
+    await expect(page).toHaveURL(/inventory.html/)
+    //check the title have text Products
+    await expect(page.locator('.title')).toHaveText('Products')
+
+    const productElement=await page.locator('.title')
+    await expect(productElement).toBeVisible()
+    //console.log(productElement)
+    //count products and print
+    const productNames=await page.$$('.inventory_item_name')
+    
+    const count=(await productNames).length
+    //printing number of element
+    console.log(`${count} are number of products`)
+    //loop through each element and print the product name
+    for(const product of productNames){
+        const name=await product.textContent()
+        console.log(name)
+    }
 })
 
 
