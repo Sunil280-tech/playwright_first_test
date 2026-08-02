2c14b76afc863d283ea46e1a2dfa98606436ff0d
Sunil <sunilrawatje2001@gmail.com>
2026-08-02 17:48:46 +0530

    new commit

--- tests/Assertion.spec.js
+++ tests/Assertion.spec.js
@@ -12,6 +12,7 @@ import {test,expect} from "@playwright/test"
     const usernameInput=page.locator('#user-name')
     const passwordInput=page.locator('#password')
     const loginButton=page.locator('#login-button')
+    const errorMessage=page.locator('[data-test="error"]')
     //varify input field username and password are visible
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
@@ -26,6 +27,8 @@ import {test,expect} from "@playwright/test"
     await expect(passwordInput).toBeEditable()
     //await expect(loginButton).toBeEditable()
 
+    expect(usernameInput).toHaveAttribute('placeholder','Username')
+
     //3.text and value assertion
     await usernameInput.fill('standard_user')
     await expect(usernameInput).toHaveValue('standard_user')
@@ -33,9 +36,30 @@ import {test,expect} from "@playwright/test"
     await passwordInput.fill('secret_sauce')
     await expect(passwordInput).toHaveValue('secret_sauce')
 
+    //verify error message to be hidden befre login attempt
+    await expect(errorMessage).toBeHidden()
+
     await loginButton.click()
     await expect(page).toHaveURL(/inventory/)
     const productTitle=page.locator('.title')
     await expect(productTitle).toBeVisible()
     await expect(productTitle).toHaveText('Products')
+
+    //class assertion
+    await expect(productTitle).toHaveClass('title')
+
+    //id assertion-verify shoppig cart id value
+    const cartIcon=page.locator('.shopping_cart_container')
+    await expect(cartIcon).toHaveId('shopping_cart_container')
+
+    //count-assertion
+    //verify total number of product displayed
+    const productItems=page.locator('.inventory_item_name')
+    await expect(productItems).toHaveCount(6)
+
+    //Screenshot visual assertion
+    //locate web element sauce lab bag pack
+
+    const bagitem=page.getByAltText('Sauce labs Backpack')
+    await expect(bagitem).toHaveScreenshot('bagItem.png')
  })
